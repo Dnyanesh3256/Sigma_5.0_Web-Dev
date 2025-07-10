50eaec5c44a4dbe75b683926e472cb3f0e8cb203
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-07-10 15:50:20 +0530

    added authorization to delete reviews ad listings

--- Backend/MajorProject/routes/review.js
+++ Backend/MajorProject/routes/review.js
@@ -2,29 +2,15 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
-const { reviewSchema } = require("../schema.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-}
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 // Add Review
-router.post("/reviews", validateReview, wrapAsync(async (req, res) => {
+router.post("/reviews", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let { id } = req.params;
-    console.log(id);
     let listing = await Listing.findById(id);
-    console.log(listing);
-
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
@@ -35,7 +21,7 @@ router.post("/reviews", validateReview, wrapAsync(async (req, res) => {
 }));
 
 //Delete Review
-router.delete("/reviews/:reviewId", wrapAsync( async (req, res) => {
+router.delete("/reviews/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync( async (req, res) => {
     let { id, reviewId } = req.params;
 
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
