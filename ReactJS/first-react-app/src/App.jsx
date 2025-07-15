f4eda9db298b736390a454bf8b29fe101c2f6f83
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-07-15 19:30:53 +0530

    started learning react js

---
+++ ReactJS/first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./Product"
+import Product from './Product'
+
+function App() {
+  return (
+    <>
+      <Product title="apna" />
+    </>
+  )
+}
+
+export default App
---
+++ ReactJS/first-react-app/src/Product.jsx
@@ -0,0 +1,11 @@
+import "./assets/Product.css";
+
+function Product({title}) {
+    return (
+        <>
+            <h2>{title}</h2>
+        </>
+    )
+}
+
+export default Product;
