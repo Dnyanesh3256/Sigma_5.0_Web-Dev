5ab94a69e4aaf285136a326ee32b2fc5aa04c62a
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-07-15 22:42:56 +0530

    learned passing arrays in props

--- ReactJS/first-react-app/src/Product.jsx
+++ ReactJS/first-react-app/src/Product.jsx
@@ -1,9 +1,11 @@
 import "./assets/Product.css";
 
-function Product({title}) {
+function Product({title, features}) {
+    let list = features.map((feature) => <li>{feature}</li>)
     return (
         <>
             <h2>{title}</h2>
+            <p>{features.map((feature) => <li>{feature}</li>)}</p>
         </>
     )
 }
