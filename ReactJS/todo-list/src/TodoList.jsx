726659c7942fed929d6fcd8d6ee7437400ead410
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-08-03 00:51:07 +0530

    added function for mark as done to tasks and completed the project

--- ReactJS/todo-list/src/TodoList.jsx
+++ ReactJS/todo-list/src/TodoList.jsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 import { v4 as uuidv4 } from "uuid"
 
 export default function TodoList(){
-    const [todos, setTodos] = useState([{id: uuidv4(), task: "sample"}]);
+    const [todos, setTodos] = useState([{id: uuidv4(), task: "sample", isDone: false}]);
     const [newTodo, setNewTodo] = useState("");
 
     const addTodo = () => {
-        setTodos([{id: uuidv4(), task: newTodo}, ...todos]);
+        setTodos([{id: uuidv4(), task: newTodo, isDone: false}, ...todos]);
         setNewTodo("");
     }
 
@@ -16,6 +16,28 @@ export default function TodoList(){
 
     const deleteTodo = (id) => {
         setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
+    const markOneAsDone = (id) => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            if(todo.id === id){
+                return{
+                    ...todo,
+                    isDone: true
+                }
+            }else{
+                return todo;
+            }
+        }))
+    }
+
+    const markAllAsDone = () => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            return{
+                ...todo,
+                isDone: true
+            }
+        }))
     }
 
     return(
@@ -31,12 +53,16 @@ export default function TodoList(){
             <ul>
                 {todos.map((todo) => (
                     <li key={todo.id}>
-                        <span>{todo.task}</span>
+                        <span style={todo.isDone ? {textDecorationLine: "line-through"} : {}}>{todo.task}</span>
                         &nbsp;&nbsp;&nbsp;&nbsp;
                         <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <button onClick={() => markOneAsDone(todo.id)}>Mark as Done</button>
                         <br /> <br />
-                    </li>
+                    </li>  
                 ))}
+
+                <button onClick={markAllAsDone}>Mark All as Done</button>
             </ul>
         </div>
     )
