54ef77c2dcab94354ef158c83c5bbe4cff2050a5
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-08-07 22:44:18 +0530

    learned dependency of useEffect hook in react

--- ReactJS/forms-in-react/src/assets/Counter.jsx
+++ ReactJS/forms-in-react/src/assets/Counter.jsx
@@ -1,20 +1,30 @@
 import { useState, useEffect } from "react"
 
 export default function Counter() {
-    const [count, setCount] = useState(0);
+    const [countX, setCountX] = useState(0);
+    const [countY, setCountY] = useState(0);
 
-    let incCount = () => {
-        setCount((prevCount) => prevCount+1);
+    let incCountX = () => {
+        setCountX((prevCountX) => prevCountX+1);
+    }
+
+    let incCountY = () => {
+        setCountY((prevCountY) => prevCountY+1);
     }
 
     useEffect(function effect() {
         console.log("useEffect is used!");
-    });
-    
+    }, [countX]);
+
     return(
         <div>
-            <h2>Count : {count}</h2>
-            <button onClick={incCount}>+1</button>
+            <h2>CountX : {countX}</h2>
+            <button onClick={incCountX}>X+1</button>
+
+            <hr />
+
+            <h2>CountY : {countY}</h2>
+            <button onClick={incCountY}>Y+1</button>
         </div>
     )
 }
