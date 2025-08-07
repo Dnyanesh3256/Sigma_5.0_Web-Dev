5667d60c021dedc674feeb186c69e995ab0c58ae
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-08-07 23:05:42 +0530

    fetched joke api and generated joke with useEffect hoock

--- ReactJS/forms-in-react/src/App.jsx
+++ ReactJS/forms-in-react/src/App.jsx
@@ -3,11 +3,12 @@ import CommentsForm from './assets/CommentsForm'
 import Form from './assets/Form'
 import Comment from './assets/Comment'
 import Counter from './assets/Counter'
+import Joke from './assets/Joke'
 
 function App() {
   return (
     <>
-      <Counter />
+      <Joke />
     </>
   )
 }
